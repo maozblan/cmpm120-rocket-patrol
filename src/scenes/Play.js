062e75ca907c49fd4e702a08023a964d82dd9583
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2024-01-22 14:19:38 -0800

    score

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -24,6 +24,21 @@ class Play extends Phaser.Scene {
         this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0)
         this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0)
 
+        // initialize score
+        this.p1Score = 0
+        let scoreConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 100
+        }
+        this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig)
     }
 
     update() {
@@ -79,5 +94,8 @@ class Play extends Phaser.Scene {
             ship.alpha = 1
             boom.destroy()
         })
+        // update score
+        this.p1Score += ship.points
+        this.scoreLeft.text = this.p1Score
     }
 }
